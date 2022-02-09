bd1b6af35f89bbb7ae2eb4c3e96d4317ccd08ab5
frsiam <siam.frs@gmail.com>
2022-02-09 17:30:35 +0600

    set condition that the count not is less then zero and create an alert the user with message

--- count.js
+++ count.js
@@ -9,6 +9,11 @@ document.getElementById('add').addEventListener('click',function(e){
 //create event listener on - button and connect with the countdown digit.
 
 document.getElementById('minus').addEventListener('click',function(e){
-    count--;
-    document.getElementById('digit').innerText = count;
+    if(count > 0){
+        count--;
+        document.getElementById('digit').innerText = count;
+    }
+    else{
+        alert("Count down can't less than zero !")
+    }
 })
