b9d0ccfe5ad34c3d0c8f8be7eecf8e47078b0051
frsiam <siam.frs@gmail.com>
2022-02-09 17:36:16 +0600

    code optimization

--- count.js
+++ count.js
@@ -3,17 +3,22 @@ let count = 0;
 
 document.getElementById('add').addEventListener('click',function(e){
     count++;
-    document.getElementById('digit').innerText = count;
-})
+    countDigit();
+});
 
 //create event listener on - button and connect with the countdown digit.
 
 document.getElementById('minus').addEventListener('click',function(e){
     if(count > 0){
         count--;
-        document.getElementById('digit').innerText = count;
+        countDigit();
     }
     else{
         alert("Count down can't less than zero !")
     }
-})
+});
+
+// catch the count digit by function
+function countDigit(){
+    document.getElementById('digit').innerText = count;
+}
