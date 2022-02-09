fb02a2ef890a8cfdd06b977c20a1da2950702876
frsiam <siam.frs@gmail.com>
2022-02-09 17:24:25 +0600

    create event listener on - button and connect with the countdown digit

--- count.js
+++ count.js
@@ -1,5 +1,14 @@
 let count = 0;
+//create event listener on + button and connect with the countdown digit.
+
 document.getElementById('add').addEventListener('click',function(e){
     count++;
+    document.getElementById('digit').innerText = count;
+})
+
+//create event listener on - button and connect with the countdown digit.
+
+document.getElementById('minus').addEventListener('click',function(e){
+    count--;
     document.getElementById('digit').innerText = count;
 })
